1c4d341693de53eacbec1eefda9073805c13d87a
Satcheen SHAKYA <Shakira@dump-accueil-1.42.fr>
2021-06-24 14:54:04 +0200

    :checkered_flag: Done !!

--- src/push_swap.c
+++ src/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: sshakya <sshakya@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/19 19:27:54 by sshakya           #+#    #+#             */
-/*   Updated: 2021/06/24 01:53:37 by sshakya          ###   ########.fr       */
+/*   Updated: 2021/06/24 14:52:21 by Shakira          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -94,7 +94,7 @@ int	main(int argc, char **argv)
 	stack_a = NULL;
 	stack_b = NULL;
 	if (argc < 2)
-		return (ps_error(stack_a));
+		return (0);
 	stack_a = ps_init_stack_a(argc, argv);
 	if (stack_a == NULL)
 		return (0);
